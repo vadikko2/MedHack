bff9bd179d01de3e99661404dd1f40829fd9a8e6
vadikko2 <vadikko2@mail.ru>
2017-11-18 02:01:46 +0300

    Add files via upload

---
+++ src/test_distances.py
@@ -0,0 +1,138 @@
+from sklearn.neighbors import DistanceMetric
+import numpy as np
+import matplotlib.pyplot as plt
+import parse
+import regression
+from sklearn.metrics.pairwise import euclidean_distances, manhattan_distances#, chebyshev_distances
+
+
+def get_only_coordinate_none_pathology(coord, dataset, params):
+    x = []
+    y = []
+    for i in range(len(dataset)):
+        if (dataset[i]['person_info']['pathology'] == 'none') and (dataset[i]['walk_info']['gait'] == 0) and (dataset[i]['person_info']['trauma'] == 'none'):
+            tmp = []
+            #print(dataset[i]['person_info']['gender'])
+            tmp.append(dataset[i]['person_info']['age'])
+            tmp.append(dataset[i]['person_info']['gender'])
+            tmp.append(dataset[i]['person_info']['height'])
+            tmp.append(dataset[i]['person_info']['feet size'])
+            tmp.append(dataset[i]['walk_info']['gait'])
+            #tmp.append(dataset[i]['walk_info']['footWear'])
+            #tmp.append(dataset[i]['walk_info']['hunger'])
+            tmp.append(dataset[i]['walk_info']['weight'])
+            for j in range(len(dataset[i]['data']) - 1):
+                tmp.append(dataset[i]['data'][j][coord])
+                #tmp.append(dataset[i]['data'][j]['y'])
+                #tmp.append(dataset[i]['data'][j]['z'])
+            x.append(np.asarray(regression.preprocess(tmp, params)))
+            y.append(dataset[i]['data'][len(dataset[i]['data']) - 1][coord])
+    return x, y
+
+
+def get_only_coordinate_pathology(coord, dataset, params):
+    x = []
+    y = []
+    for i in range(len(dataset)):
+        if (dataset[i]['person_info']['pathology'] != 'none') and (dataset[i]['walk_info']['gait'] == 0) and (dataset[i]['person_info']['trauma'] == 'none'):
+            tmp = []
+            #print(dataset[i]['person_info']['gender'])
+            tmp.append(dataset[i]['person_info']['age'])
+            tmp.append(dataset[i]['person_info']['gender'])
+            tmp.append(dataset[i]['person_info']['height'])
+            tmp.append(dataset[i]['person_info']['feet size'])
+            tmp.append(dataset[i]['walk_info']['gait'])
+            #tmp.append(dataset[i]['walk_info']['footWear'])
+            #tmp.append(dataset[i]['walk_info']['hunger'])
+            tmp.append(dataset[i]['walk_info']['weight'])
+            for j in range(len(dataset[i]['data']) - 1):
+                tmp.append(dataset[i]['data'][j][coord])
+                #tmp.append(dataset[i]['data'][j]['y'])
+                #tmp.append(dataset[i]['data'][j]['z'])
+            x.append(np.asarray(regression.preprocess(tmp, params)))
+            y.append(dataset[i]['data'][len(dataset[i]['data']) - 1][coord])
+    return x, y
+
+p = parse.Parser('/home/vadim/hackatones/medhack/data/')
+p.parse_path(100)
+p.delete_from_back(500)
+dataset = p.get_split_database(200)
+p.edit_features()
+print(len(dataset)
+
+x_none, _ = get_only_coordinate_none_pathology('x', dataset, ['arctn'])
+x, _ = get_only_coordinate_pathology('x', dataset, ['arctn'])
+x_tmp = x[::-1]
+x_tmp_none = x_none[::-1]
+points = []
+
+'''Manhattan'''
+plt.subplot(3,3,1)
+for i in range(len(x)):
+    points.append(manhattan_distances([x_none[i]], [x[i]]))
+plt.scatter(range(len(points)), points)
+plt.title('Manhattan 1-0')
+
+point = []
+plt.subplot(3,3,2)
+for i in range(len(x)):
+    points.append(manhattan_distances([x_none[i]], [x_tmp_none[i]]))
+plt.scatter(range(len(points)), points)
+plt.title('Manhattan 0-0')
+
+point = []
+plt.subplot(3,3,3)
+for i in range(len(x)):
+    points.append(manhattan_distances([x[i]], [x_tmp[i]]))
+plt.scatter(range(len(points)), points)
+plt.title('Manhattan 1-1')
+
+'''Euclidean'''
+plt.subplot(3,3,4)
+for i in range(len(x)):
+    points.append(euclidean_distances([x_none[i]], [x[i]]))
+plt.scatter(range(len(points)), points)
+plt.title('Euclidean 1-0')
+
+point = []
+plt.subplot(3,3,5)
+for i in range(len(x)):
+    points.append(euclidean_distances([x_none[i]], [x_tmp_none[i]]))
+plt.scatter(range(len(points)), points)
+plt.title('Euclidean 0-0')
+
+point = []
+plt.subplot(3,3,6)
+for i in range(len(x)):
+    points.append(euclidean_distances([x[i]], [x_tmp[i]]))
+plt.scatter(range(len(points)), points)
+plt.title('Euclidean 1-1')
+
+'''ChebyshevDistance
+plt.subplot(3,3,7)
+for i in range(len(x)):
+    points.append(chebyshev_distances([x_none[i]], [x[i]]))
+plt.scatter(range(len(points)), points)
+plt.title('ChebyshevDistance 1-0')
+
+point = []
+plt.subplot(3,3,8)
+for i in range(len(x)):
+    points.append(chebyshev_distances([x_none[i]], [x_tmp_none[i]]))
+plt.scatter(range(len(points)), points)
+plt.title('ChebyshevDistance 0-0')
+
+point = []
+plt.subplot(3,3,9)
+for i in range(len(x)):
+    points.append(chebyshev_distances([x[i]], [x_tmp[i]]))
+plt.scatter(range(len(points)), points)
+plt.title('ChebyshevDistance 1-1')
+'''
+plt.show()
+
+
+
+
+
+#
