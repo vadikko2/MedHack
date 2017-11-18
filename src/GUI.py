d592e6ef5dae89f8a9c416bafea2e4887e7f8c56
Aleksey Zausalin <zausalin2@gmail.com>
2017-11-18 23:30:22 +0300

    Major GUI update

--- src/GUI.py
+++ src/GUI.py
@@ -40,23 +40,24 @@ class MainWindow(QWidget):
         self.path_to_save_label = QLabel('Path to save Result', self)
         self.path_to_save_field = QLineEdit(self)
 
-        self.preview_button = QPushButton('View Plot',self)
+        self.prediction_label = QLabel('Path to file with Accelerometer data',self)
+        self.prediction_field = QLineEdit(self)
+        self.predict_button = QPushButton('New prediction',self)
+        self.view_progress_button = QPushButton('View Progress',self)
+        self.preview_button = QPushButton('View Diagnosis',self)
         self.saveTemplate_button = QPushButton('Save template',self)
 
-        self._dataPath = 'None'
-        self._userName = 'None'
-        self._classifier = 'None'
-        self._regression = 'None'
-        self._pathToSave = 'None'
+        self._dataPath = ''
+        self._userName = 'NAME'
+        self._classifier = 'Classifier'
+        self._regression = 'Regression'
+        self._pathToSave = ''
+        self._preditPath = ''
         
         self.canvas = FigureCanvas(self.figure)
-        self.plot_label = QLabel('Probabilities plot', self)
+        self.plot_label = QLabel('Information plot', self)
         self.log_text_field = QTextEdit(self)
         self.log_prev_label = QLabel('Log Aidut',self)
-
-        self.export_button = QPushButton('Export', self)
-
-        self.data = 'No action'
 
         self.initUI()
 
@@ -67,9 +68,13 @@ class MainWindow(QWidget):
 
         left_layout = QGridLayout()
 
-        self.preview_button.clicked.connect(self.viewPlot)
+        self.preview_button.clicked.connect(self.view_plt)
+        self.view_progress_button.clicked.connect(self.view_plt)
+
         self.saveTemplate_button.clicked.connect(self.save_template)
         self.input_param_button.clicked.connect(self.load_data)
+
+        self.predict_button.clicked.connect(self.prediction)
 
         left_layout.addWidget(self.input_param,0,0,1,1)
         left_layout.addWidget(self.input_param_button,0,1,1,1)
@@ -85,8 +90,14 @@ class MainWindow(QWidget):
         left_layout.addWidget(self.path_to_save_label,5,0,1,1)
         left_layout.addWidget(self.path_to_save_field,5,1,1,1)
 
-        left_layout.addWidget(self.saveTemplate_button,6,0,1,1)
-        left_layout.addWidget(self.preview_button,6,1,1,1)
+        left_layout.addWidget(self.preview_button,6,0,1,1)
+        left_layout.addWidget(self.view_progress_button,6,1,1,1)
+
+        left_layout.addWidget(self.prediction_label,7,0,1,2)
+        left_layout.addWidget(self.prediction_field,8,0,1,2)
+
+        left_layout.addWidget(self.predict_button,9,0,1,1)
+        left_layout.addWidget(self.saveTemplate_button,9,1,1,1)
 
         right_layout = QGridLayout()
 
@@ -102,11 +113,9 @@ class MainWindow(QWidget):
 
         self.log_text_field.setReadOnly(True)
 
-        self.export_button.clicked.connect(self.saveData)
-
         log_layout.addWidget(self.log_prev_label,0,0,1,1)
-        log_layout.addWidget(self.log_text_field,1,0,1,2)
-        log_layout.addWidget(self.export_button,2,1,1,1)
+        log_layout.addWidget(self.log_text_field,1,0,1,4)
+        
 
         layout = QVBoxLayout()
         layout.addLayout(main_layout)
@@ -115,6 +124,31 @@ class MainWindow(QWidget):
         self.setLayout(layout)
         self.show()
 
+
+    def prediction(self):
+
+        self.readAllData()
+
+        result = 'Nothing'
+        name = 'NAME'
+        '''
+        name, data = прочитать файл по пути (self._preditPath) вернуть имя и всю дату для регрессии
+            
+        result = регрессия(data)
+
+        if result == 'болен':
+            result += SVM(data)
+            
+            сохранить в self._pathToSave + 'data.csv'
+
+            self.viewPlot('View Diagnosis')
+
+
+ 
+        '''
+        self._userName = name
+
+        self.viewEvent(self._userName + ' have a ' + result)
 
     def load_data(self):
 
@@ -128,6 +162,7 @@ class MainWindow(QWidget):
                 self.class_field.setText(conf["Classifier"])
                 self.regr_field.setText(conf["Regression"])
                 self.path_to_save_field.setText(conf["Output"])
+                self.prediction_field.setText(conf["PredictPath"])
                 
         except Exception as e:
             self.viewEvent("Error: " + str(e))
@@ -145,42 +180,113 @@ class MainWindow(QWidget):
         f.write('  \"User\": \"' + self.name_field.text() + '\",\n')
         f.write('  \"Classifier\": \"' + self.class_field.text()+ '\",\n')
         f.write('  \"Regression\": \"' + self.regr_field.text()+ '\",\n')
-        f.write('  \"Output\": \"' + self.path_to_save_field.text()+ '\"\n')
+        f.write('  \"Output\": \"' + self.path_to_save_field.text()+ '\",\n')
+        f.write('  \"PredictPath\": \"' + self.prediction_field.text()+ '\"\n')
         f.write('\n}')
 
         self.viewEvent('All data saved in ' + self.template_file_name + ' file')
 
         f.close()
 
-    def viewPlot(self):
+    def load_data_base(self, name, _type = 1):
+
+        f = []
+
+        y = []
+        x_label = []
+
+        try:
+            f = open(self._dataPath + 'data.csv', 'r')
+        except:
+            self.viewEvent('Missing data.csv file in' + self._dataPath + ' directy!')
+            return y,x_label
+
+        findFlag = False
+
+        for line in f:
+            line = line.rstrip('\n').split(';')
+            
+            if line[0] == name:
+                
+                data = line[_type].split(',')
+
+                findFlag = True
+
+                for k in data:
+                    k = k.split('-')
+                    y.append(float(k[1]))
+                    x_label.append(k[0])
+
+                break
+        
+        f.close()
+
+        return y, x_label
+
+
+    def view_plt(self):
+
+        source = self.sender()
+        self.viewPlot(source.text())
+
+
+    def viewPlot(self, action):
 
         self.readAllData()
 
         self.figure.clear()
 
         ax = self.figure.add_subplot(111)
+       
+        if action == 'View Progress':
 
-        x = [ i for i in range(0,100)]
-        y = [ i**2 for i in range(0,100)]
+            y,x_label = self.load_data_base(self._userName, _type = 2)
+
+            if len(y) == 0:
+                self.viewEvent('This person (' + self._userName + ') don\'t have a info in data base!')
+                return False
+
+            x = [ i for i in range(0, len(x_label))]
+
+            ax.plot(y)
+
+            ax.set_xticks(x)
+            ax.set_xticklabels(x_label)
+            ax.set_yticks([])
+
+            ax.set_xlabel('Time')
+            ax.set_ylabel('Distanse for normal steps')
+            ax.set_xticklabels(x_label)
+            ax.set_title('Reabilitation progress')
         
-        ax.plot(x,y)
+            self.canvas.draw()
 
-        self.canvas.draw()
+            self.viewEvent("Preview plot was build")
 
-        self.viewEvent("Preview plot was build")
+        elif action == 'View Diagnosis':
 
-    def saveData(self):
+            y,x_label = self.load_data_base(self._userName, _type = 1)
 
-        self.readAllData()
+            if len(y) == 0:
+                self.viewEvent('This person (' + self._userName + ') don\'t have a info in data base!')
+                return False
 
-        f = open(self._pathToSave,'w')
+            x = [ i for i in range(0, len(x_label))]
+            
+            ax.bar(x,y)
 
-        for line in self.data:
-            f.write(line + '\n')
+            ax.set_xticks(x)
+            ax.set_xticklabels(x_label)
+            ax.set_yticks([])
 
-        f.close()
+            ax.set_xlabel('Diagnosis')
+            ax.set_ylabel('Probobility')
+            ax.set_title('Diagnosis recomendation')
+        
+            self.canvas.draw()
 
-        self.viewEvent("CEF export to " + self._pathToSave + " complited!")
+            self.viewEvent("Preview plot was build")
+
 
     def readAllData(self):
         try:
@@ -190,6 +296,7 @@ class MainWindow(QWidget):
             self._classifier = self.class_field.text()
             self._regression = self.regr_field.text()
             self._pathToSave = self.path_to_save_field.text()
+            self._preditPath = self.prediction_field.text()
 
             line = self._pathToSave.split('/')
 
